da68838fcf1ff28ab68ad68bfa230e35f4b27e32
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-24 14:42:18 +0530

    feat: setup routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,21 @@
 import App from "./App";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
 const routes = [
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "products",
+    element: <Products />,
+  },
+  {
+    path: "cart",
+    element: <Cart />,
+  },
+
 ];
 
 export default routes;
