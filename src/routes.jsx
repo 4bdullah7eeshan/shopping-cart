f9b355042a18b02181d4995a6238fef896044e27
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-24 15:42:02 +0530

    feat: add an error pages
    Rework on this later.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import App from "./App";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
+import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "products",
