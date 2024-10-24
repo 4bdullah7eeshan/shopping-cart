c092b1eab4df4e5a2411eefc4435a09e4373311e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-24 14:38:09 +0530

    feat: setup carts page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import CartMain from "../components/CartMain";
+import Footer from "../components/Footer"
+
+function Cart() {
+    return (
+        <>
+            <Header />
+            <CartMain />
+            <Footer />
+        </>
+    );
+}
+
+export default Cart;
