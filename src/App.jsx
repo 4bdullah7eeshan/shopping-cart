568e6a02cbdcf879e372e879831221fe41b5eaa3
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-24 14:40:58 +0530

    refactor: move the homepage to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Header from "./components/Header"
+import HomeMain from "./components/HomeMain"
+import Footer from "./components/Footer"
+
 function App() {
-  
   return (
     <>
-      
+        <Header />
+        <HomeMain />
+        <Footer />
     </>
-  )
+  );
 }
 
 export default App
