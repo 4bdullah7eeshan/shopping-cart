323e9fa3afb0e5dab70acb0301fb36596f40bd5e
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-24 14:37:20 +0530

    feat: setup products page

---
+++ src/pages/Products.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import ProductsMain from "../components/ProductsMain";
+import Footer from "../components/Footer"
+
+function Products() {
+    return (
+        <>
+            <Header />
+            <ProductsMain />
+            <Footer />
+        </>
+    );
+}
+
+export default Products;
