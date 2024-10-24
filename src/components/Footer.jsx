d3789b1a3a7ff11c4fc7ac8b9917f77fff620aeb
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-24 14:17:57 +0530

    feat: create a Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        // Add more stuff just like a typical shopping app would have
+        <footer>
+            <div>
+                <a href="https://github.com/4bdullah7eeshan" target="_blank" rel="noopener noreferrer">
+                    Copyright &copy; Abdullah Zeeshan 2024
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
