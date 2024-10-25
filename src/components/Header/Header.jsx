af1848f9577e21c9dc9b5942b621c7163c91d1b4
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-25 10:46:58 +0530

    feat(style): setup header alignment

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import styles from "./Header.module.css";
 
 function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <Logo />
             <Nav />
         </header>
