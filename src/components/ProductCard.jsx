0599461f4585d84b87588461c85cd27a91ce4065
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-25 10:16:35 +0530

    wip: setup a ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+function ProductCard() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <p>Product Name</p>
+            <p>Price</p>
+            <button>Cost</button>
+        </div>
+    );
+}
+
+export default ProductCard;
