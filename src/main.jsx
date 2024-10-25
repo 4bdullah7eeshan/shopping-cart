91d502228def4a51076b43c2716f81ca8b197e6f
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-25 11:04:59 +0530

    feat(style): apply resets

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
-import './index.css'
+import "./styles/reset.module.css";
 
 const router = createBrowserRouter(routes);
 
