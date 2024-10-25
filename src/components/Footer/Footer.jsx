a8c7e6432df6b3f1580c4ce1c20383bf02036045
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-25 10:58:00 +0530

    feat(style): setup footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,17 @@
 import { Github } from 'lucide-react';
+import styles from "./Footer.module.css";
 
 function Footer() {
     return (
         // Add more stuff just like a typical shopping app would have
-        <footer>
+        <footer className={styles.footer}>
             <div>
-                <div>
-                    <p>Copyright &copy; Abdullah Zeeshan 2024</p>
-                </div>
-                <div>
-                    <a href="https://github.com/4bdullah7eeshan" target="_blank" rel="noopener noreferrer">
-                        <Github />
-                    </a>
-                </div>
+                <p>Copyright &copy; Abdullah Zeeshan 2024</p>
+            </div>
+            <div>
+                <a href="https://github.com/4bdullah7eeshan" target="_blank" rel="noopener noreferrer">
+                    <Github />
+                </a>
             </div>
         </footer>
     );
