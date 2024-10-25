b2ee565a2f7d8a019d85a67da167148540d0753f
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-25 10:55:15 +0530

    refactor: add github icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
+import { Github } from 'lucide-react';
+
 function Footer() {
     return (
         // Add more stuff just like a typical shopping app would have
         <footer>
             <div>
-                <a href="https://github.com/4bdullah7eeshan" target="_blank" rel="noopener noreferrer">
-                    Copyright &copy; Abdullah Zeeshan 2024
-                </a>
+                <div>
+                    <p>Copyright &copy; Abdullah Zeeshan 2024</p>
+                </div>
+                <div>
+                    <a href="https://github.com/4bdullah7eeshan" target="_blank" rel="noopener noreferrer">
+                        <Github />
+                    </a>
+                </div>
             </div>
         </footer>
     );
