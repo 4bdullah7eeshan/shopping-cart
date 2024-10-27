16d3e0868ec4b19ebcf208abb820fd4f3f7f36b0
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-27 12:53:40 +0530

    feat: add functionality to '+' and '-' btns

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,28 +1,39 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 
 const ProductCard = ({ product }) => {
-  return (
-    <div className={styles.card}>
-      <img src={product.image} alt={product.title} className={styles.productImage} />
-      <h3>{product.title}</h3>
-      <p>${product.price}</p>
-      <div>
-        <button>-</button>
-        <input type="number" defaultValue={1} min={1} /> {/* improve this */}
-        <button>+</button>
-      </div>
-      <button>Add to Cart</button> {/* create a diff component */}
-    </div>
-  );
+    const [quantity, setQuantity] = useState(0);
+
+    const handleIncrement = () => {
+        setQuantity((prevQuantity) => prevQuantity + 1);
+    };
+
+    const handleDecrement = () => {
+        setQuantity((prevQuantity) => Math.max(prevQuantity - 1, 0));
+    };
+
+    return (
+        <div className={styles.card}>
+        <img src={product.image} alt={product.title} className={styles.productImage} />
+        <h3>{product.title}</h3>
+        <p>${product.price}</p>
+        <div>
+            <button onClick={handleDecrement}>-</button>
+            <input type="number" value={quantity} min={0} readOnly/>
+            <button onClick={handleIncrement}>+</button>
+        </div>
+        <button>Add to Cart</button> {/* create a diff component */}
+        </div>
+    );
 };
 
 ProductCard.propTypes = {
-  product: PropTypes.shape({
-    image: PropTypes.string,
-    title: PropTypes.string,
-    price: PropTypes.number,
-  }).isRequired,
+    product: PropTypes.shape({
+        image: PropTypes.string,
+        title: PropTypes.string,
+        price: PropTypes.number,
+    }).isRequired,
 };
 
 export default ProductCard;
