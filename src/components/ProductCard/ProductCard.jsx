f37b77015addcec460f7238baa7cf8c93478f766
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-27 12:46:14 +0530

    feat(products): fetch products and render them

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,29 @@
-function ProductCard() {
-    return (
-        <div>
-            <img src="" alt="" />
-            <p>Product Name</p>
-            <p>Price</p>
-            <button>Cost</button>
-        </div>
-    );
-}
+import PropTypes from "prop-types";
+import styles from "./ProductCard.module.css";
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className={styles.card}>
+      <img src={product.image} alt={product.title} className={styles.productImage} />
+      <h3>{product.title}</h3>
+      <p>${product.price}</p>
+      <div>
+        <button>-</button>
+        <input type="number" defaultValue={1} min={1} /> {/* improve this */}
+        <button>+</button>
+      </div>
+      <button>Add to Cart</button> {/* create a diff component */}
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    image: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+  }).isRequired,
+};
 
 export default ProductCard;
+
