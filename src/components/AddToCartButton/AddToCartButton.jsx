b078a55d4de15d976b2b6387f3b573cef8d19416
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-27 15:57:29 +0530

    wip(cart): create an add to cart btn component

---
+++ src/components/AddToCartButton/AddToCartButton.jsx
@@ -0,0 +1,24 @@
+import { useCart } from "../context/CartContext";
+import PropTypes from "prop-types";
+import styles from "./AddToCartButton.module.css";
+
+const AddToCartButton = ({ product, quantity }) => {
+    const { addToCart } = useCart();
+
+    const handleAddToCart = () => {
+        addToCart(product, quantity);
+    };
+
+    return (
+        <button onClick={handleAddToCart} className={styles.addToCartButton}>
+            Add to Cart
+        </button>
+    );
+};
+
+AddToCartButton.propTypes = {
+    product: PropTypes.object.isRequired,
+    quantity: PropTypes.number.isRequired,
+};
+
+export default AddToCartButton;
