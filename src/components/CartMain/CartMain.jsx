356747bca9824ee49ce214751e18d48ace35a91f
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-29 14:32:04 +0530

    wip(cart): render cart page

--- src/components/CartMain/CartMain.jsx
+++ src/components/CartMain/CartMain.jsx
@@ -1,13 +1,38 @@
+import PropTypes from "prop-types";
 import styles from "./CartMain.module.css";
 
-function CartMain() {
-
-    return (
-        <main>
-            <h1>Shopping Cart</h1>
-            
-        </main>
-    );
+function CartMain({ cart }) {
+  return (
+    <main>
+      <h1>Shopping Cart</h1>
+      {cart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cart.map((item) => (
+            <li key={item.id}>
+              <img src={item.image} alt={item.title} />
+              <h3>{item.title}</h3>
+              <p>${item.price}</p>
+              <p>Quantity: {item.quantity}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </main>
+  );
 }
+
+CartMain.propTypes = {
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      image: PropTypes.string,
+      title: PropTypes.string,
+      price: PropTypes.number,
+      quantity: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default CartMain;
