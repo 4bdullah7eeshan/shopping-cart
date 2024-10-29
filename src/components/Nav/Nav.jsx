3161e8e7909bcd52716454e1bac8809219f994d8
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-29 14:28:20 +0530

    wip(cart): include item count in nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from "./Nav.module.css";
 
-function Nav() {
+function Nav({ cartItemCount }) {
     return (
         <nav className={styles.nav}>
             <Link to="/">Home</Link>
             <Link to="/products">Products</Link>
-            <Link to="/cart">Cart ()</Link> {/* Here, the count of items in cart must be shown too! */}
+            <Link to="/cart">Cart ({cartItemCount})</Link> {/* Here, the count of items in cart must be shown too! */}
         </nav>
     );
 }
