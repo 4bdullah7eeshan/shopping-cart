06232d1aab6e5d00391fcd2887659e6e6abceac4
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-10-31 15:36:07 +0530

    feat(categ): place categories for products

--- src/components/ProductsMain/ProductsMain.jsx
+++ src/components/ProductsMain/ProductsMain.jsx
@@ -1,10 +1,16 @@
 // ProductsMain.jsx
+import { useState } from "react";
 import { useCart } from "../../context/CartContext";
 import ProductCard from "../ProductCard/ProductCard";
 import styles from "./ProductsMain.module.css";
 
 function ProductsMain() {
-  const { products, error, loading, addToCart } = useCart();
+  const { products, categories, error, loading, addToCart } = useCart();
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const filteredProducts = selectedCategory === "all" 
+    ? products 
+    : products.filter(product => product.category === selectedCategory);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
@@ -17,12 +23,20 @@ function ProductsMain() {
       <div className={styles.view}>
         <aside>
           <h3>Categories</h3>
+          <ul>
+            <li onClick={() => setSelectedCategory("all")}>All</li>
+            {categories.map((category, index) => (
+              <li key={index} onClick={() => setSelectedCategory(category)}>
+                {category}
+              </li>
+            ))}
+          </ul>
         </aside>
         <section className={styles.products}>
-        {products && products.map((product) => (
-          <ProductCard key={product.id} product={product} addToCart={addToCart} />
-        ))}
-      </section>
+          {filteredProducts && filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} addToCart={addToCart} />
+          ))}
+        </section>
       </div>
     </main>
   );
