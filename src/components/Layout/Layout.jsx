779d54bb4a5409007586ef62d718a19f7ea98f5d
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-02 16:56:45 +0530

    refactor: remove preHeader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import PreHeader from "../PreHeader/PreHeader";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { useCart } from "../../context/CartContext";
@@ -12,7 +11,6 @@ const Layout = () => {
 
   return (
     <>
-      <PreHeader />
       <Header cartItemCount={cartItemCount} />
       <Outlet />
       <Newsletter />
