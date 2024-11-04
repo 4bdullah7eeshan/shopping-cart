05cba5ab6e856b316212ff7e47c85321227c1141
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-04 17:00:36 +0530

    refactor: carve out Carousel into a component

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,39 @@
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+function Carousel () {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    };
+
+    const images = [
+        "https://cdn.pixabay.com/photo/2017/08/06/17/58/people-2594683_1280.jpg",
+        "https://cdn.pixabay.com/photo/2017/09/01/21/53/sunglasses-2705642_1280.jpg",
+        "https://cdn.pixabay.com/photo/2017/03/27/13/28/man-2178721_1280.jpg",
+        "https://cdn.pixabay.com/photo/2022/02/12/21/37/woman-7009979_1280.jpg",
+        "https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_1280.jpg"
+    ];
+
+    return (
+        <>
+            <Slider {...settings}>
+            {images.map((img, index) => (
+                <div key={index}>
+                    <img src={img} alt={`Slide ${index}`} style={{ width: "100%", height: "auto" }} />
+                </div>
+            ))}
+            </Slider>
+        </>
+    )
+
+}
+
+export default Carousel;
